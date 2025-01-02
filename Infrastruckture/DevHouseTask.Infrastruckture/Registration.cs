50a26eb1332d6a10baecb9fd3629e354383dd012
Oğuzhan Ergin <oguz_han_er_gin11@hotmail.com>
2025-01-03 00:52:55 +0300

    token service tamamlandı ve swaggerda auth olma işlemi eklendi

--- Infrastruckture/DevHouseTask.Infrastruckture/Registration.cs
+++ Infrastruckture/DevHouseTask.Infrastruckture/Registration.cs
@@ -1,6 +1,9 @@
-﻿using DevHouseTask.Infrastruckture.Tokens;
+﻿using DevHouseTask.Application.Interfaces.Tokens;
+using DevHouseTask.Infrastruckture.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +17,26 @@ namespace DevHouseTask.Infrastruckture
         public static void AddInfrastruckture(this IServiceCollection services,IConfiguration configuration) 
         {
             services.Configure<TokenSettings>(configuration.GetSection("JWT"));
+            services.AddTransient<ITokenService, TokenService>();
+
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme,opt=>
+            {
+                opt.SaveToken = true;
+                opt.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"])),
+                    ValidIssuer = configuration["JWT:Issuer"],
+                    ValidAudience = configuration["JWT:Audience"]
+                };
+            }
+            );
         }
     }
 }
--- Presentation/DevHouseTask.Api/Program.cs
+++ Presentation/DevHouseTask.Api/Program.cs
@@ -2,6 +2,7 @@ using DevHouseTask.Application;
 using DevHouseTask.Persistence;
 using DevHouseTask.Infrastruckture;
 using DevHouseTask.Mapper;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,35 @@ builder.Services.AddPersistence(builder.Configuration);
 builder.Services.AddInfrastruckture(builder.Configuration);
 builder.Services.AddApplication();
 builder.Services.AddCustomMapper();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "DevHouseTask Api", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "'Bearer' yazýp boþluk býraktýktan sonra Token'ý kullanabilirsiniz"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+ 
+    });
+});
 
 var app = builder.Build();
 
